f98cb181fbaf125344230d8c9588e23b1a1154fa
Adam Rudy <38328889+arudy05@users.noreply.github.com>
2024-10-07 20:53:36 -0600

    v0.2
    **0.2: The Friends Update (part 1)**
    - You can now enter names for players
    - Scores for each player are displayed underneath the grid
        - only player 1 can earn points

--- spyanswer.py
+++ spyanswer.py
@@ -2,16 +2,23 @@ import matplotlib.pyplot as plt
 
 def game():
     while sum(answer1)+sum(answer2)+sum(answer3)+sum(answer4)+sum(answer5) != 0:    
+        #Format and display score and grid
         tablecells = [categories, answer1, answer2, answer3, answer4, answer5 ]
-        plt.figure(figsize=(12,6),dpi=100)
-        grid = plt.table(tablecells,loc='center',cellLoc='center')
-        grid.scale(1,6)
+        scorecells = [players, scores]
+        plt.figure(figsize=(12,2),dpi=100)
+        grid = plt.table(tablecells,loc='top',cellLoc='center')
+        grid.scale(2,8)
         grid.auto_set_font_size(False)
-        grid.set_fontsize(16)
+        grid.set_fontsize(24)
+        score = plt.table(scorecells,loc='bottom',cellLoc='center')
+        score.scale(2,6)
+        score.auto_set_font_size(False)
+        score.set_fontsize(24)
         plt.gca().get_xaxis().set_visible(False)
         plt.gca().get_yaxis().set_visible(False)
         plt.box(on=None)
         plt.show()
+        #the gameplay
         cat = int(input("Select a category (1-6, L-R): "))
         if cat < 1 or cat > 6:
             print("Not a valid category!")
@@ -21,22 +28,33 @@ def game():
                 print("Tile already selected!")
             elif wager == rnddiv:
                 answer1[cat-1] = 0
+                scores[0] = scores[0] + rnddiv
             elif wager == rnddiv*2:
                 answer2[cat-1] = 0
+                scores[0] = scores[0] + rnddiv*2
             elif wager == rnddiv*3:
                 answer3[cat-1] = 0
+                scores[0] = scores[0] + rnddiv*3
             elif wager == rnddiv*4:
                 answer4[cat-1] = 0
+                scores[0] = scores[0] + rnddiv*4
             elif wager == rnddiv*5:
                 answer5[cat-1] = 0
+                scores[0] = scores[0] + rnddiv*5
             else:
                 print("Not a valid wager!")
         print()
     print("All questions selected! Game over!")
 
 categories = ["These","Are","Not","Actual","Jeopardy","Categories"]
-print("This.. is... JEOPARDY!")
+players = []
+scores = [0,0,0]
+print("This.. is... JEOPARDY!\n")
+for i in range(3):
+    pname = str(input("Player %i: " % (i+1)))
+    players += [pname]
 rnd = int(input("What round would you like to play? "))
+print()
 if rnd == 1:
     answer1 = [200,200,200,200,200,200]
     answer2 = [400,400,400,400,400,400]
