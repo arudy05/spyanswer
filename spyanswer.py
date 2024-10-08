5785db1bbf88f9f4e2b8b7be6b8f068832b4b1ff
Adam Rudy <38328889+arudy05@users.noreply.github.com>
2024-10-07 20:59:23 -0600

    v1.0
    **1.0: The Update In Which The Game Takes Shape**
    - Removed 2nd round – will readd later once I can have the program read a second set of categories/questions/answers
    - Now pulls categories and questions from file “categories.json”
        - JSON is now used over CSV – this means that I can have things organized in a way that is Very flexible
    - Added the capacity to actually answer questions
        - Answers are stored in a dictionary in a key associated with the question name
        - Multiple answers are possible for each question (abbreviations, numbers/letters, capitalizations, etc.)
        - Wrong answer? Lose points!
        - Correct answer? Gain points!
    - Variables col1, col2, col3, col4 and col5 have been merged into one “tiles” variable
        - This makes the overall code a bit less garbage

--- spyanswer.py
+++ spyanswer.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# SPYANSWER! - version 0.9                                                    #
+# SPYANSWER! - version 1.0                                                    #
 # an answer-and-question trivia game (like Jeopardy!) for the Spyder IDE      #
 # by Adam Rudy (arudy@ualberta.ca)                                            #
 # written for the ENCMP 100 Programming Contest at the University of Alberta  #
@@ -10,14 +10,12 @@ import matplotlib.font_manager as font
 import numpy as np
 import time
 import sys
-import csv
+import json
 
 ## Global variables kept here for efficiency, etc
-data = []
 categories = []
 questions = []
 scores = [0,0,0]
-pBuzz = 0
 catSet = font.FontProperties(size="24", weight="bold")
 tileSet = font.FontProperties(size="36", weight="bold")
 gridcolours = np.full((6,6), "#010D8C")
@@ -31,7 +29,7 @@ def main(): # The heart and soul (not really) of the program
     time.sleep(1.5) # huge ascii block coming up. woah
     print("\r███████╗██████╗ ██╗   ██╗ █████╗ ███╗   ██╗███████╗██╗    ██╗███████╗██████╗ ██╗\n██╔════╝██╔══██╗╚██╗ ██╔╝██╔══██╗████╗  ██║██╔════╝██║    ██║██╔════╝██╔══██╗██║\n███████╗██████╔╝ ╚████╔╝ ███████║██╔██╗ ██║███████╗██║ █╗ ██║█████╗  ██████╔╝██║\n╚════██║██╔═══╝   ╚██╔╝  ██╔══██║██║╚██╗██║╚════██║██║███╗██║██╔══╝  ██╔══██╗╚═╝\n███████║██║        ██║   ██║  ██║██║ ╚████║███████║╚███╔███╔╝███████╗██║  ██║██╗\n╚══════╝╚═╝        ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝ ╚══════╝╚═╝  ╚═╝╚═╝")
     time.sleep(0.5) 
-    print("Version %-11s %60s\n" % ("0.9", "Written by Adam Rudy"))
+    print("Version %-11s %60s\n" % ("1.0", "Written by Adam Rudy"))
     
     # The menu!
     choice = menu()
@@ -47,7 +45,6 @@ def main(): # The heart and soul (not really) of the program
                 pname = str(input("Player %d > " % (i+1)))
                 players += [pname]
             round(1)
-            round(2)
             print("[========= GAME OVER! =========]") 
         else:
             print("Invalid choice!\n")
@@ -56,8 +53,8 @@ def main(): # The heart and soul (not really) of the program
 
 def menu(): # The program's "main menu"
     print("[========== MAIN MENU ==========]")
-    print("    1           Start Game")
-    print("    0           Exit")
+    print("%5s %22s" % ("1", "Start Game"))
+    print("%5s %22s" % ("0", "Exit"))
     choice = input("Select an option > ")
     while not choice.isdigit():
         choice = input("Select an option > ")
@@ -65,24 +62,32 @@ def menu(): # The program's "main menu"
     return choice
 
 def round(num): #This function handles the basic gameplay loop
+
+    answers = {} # this cannot be a global variable purely because it would be too easily accessible from Spyder's variable explorer
     
-    with open('text0.9.csv') as file: # gets categories and questions from file 'text.csv'
-        reader = csv.reader(file)
-        for row in reader:
-            data.append(row)
-    for i in range(6):
-        categories.append(data[i+1][0])
-        questions.append([data[i+1][1],data[i+1][2],data[i+1][3],data[i+1][4],data[i+1][5]])    
-    
+    with open("categories.json", 'r') as file: # the file that contains categories, answers, and questions
+        a = json.load(file)
+        if a["key"] != "trebek": # checks to make sure that the JSON file is valid by looking for a "key" of sorts
+            sys.exit("ERROR! Invalid JSON file.") # if the key is not present, the program ends
+        else:
+            print("Valid JSON file!")
+            
+            for i in range(len(a["category"])):
+                questemp = [] # temporary variable, cleared every iteration
+                b = str(i+1)
+                categories.append(a["category"][b]["name"]) # fetches all categories and puts them in a list
+                for j in range(len(a["category"][b])-1):
+                    c = str(j+1)
+                    questemp.append(a["category"][b][c]["name"]) # fetches all questions for a category and puts them in a temporary list
+                    answers[a["category"][b][c]["name"]] = a["category"][b][c]["answers"] 
+                questions.append(questemp) # adds this category's questions as a new list within the questions list
+                
     rnddiv = 200*num
-    col1 = [rnddiv*1] * 6
-    col2 = [rnddiv*2] * 6
-    col3 = [rnddiv*3] * 6
-    col4 = [rnddiv*4] * 6
-    col5 = [rnddiv*5] * 6
+    tiles = [categories, [rnddiv*1]*6, [rnddiv*2]*6, [rnddiv*3]*6, [rnddiv*4]*6, [rnddiv*5]*6]
     print("[======= ROUND %d START!! =======]\n" % num)
     
-    (tablecells, cellsum) = graphics(col1, col2, col3, col4, col5) # initial rendering of the board
+    (tablecells, cellsum) = graphics(tiles) # initial rendering of the board
+    pBuzz = 0
     pBoard = 0
     # Checking that there are still spaces on the board left
     while cellsum != 0:        
@@ -104,39 +109,49 @@ def round(num): #This function handles the basic gameplay loop
             wager = input("For how much? > ")
             
             try: # more error handling :)
-                indexes = int(wager)/rnddiv # important
+                indexes = int(wager)/rnddiv # used to check to see if the wager is actually on the board, among other things
             except ValueError:
                 error = True
             
             if error:
                 errors(2)
-            elif indexes < 1 or indexes > 5:
+            elif indexes < 1 or indexes > 5: # wager too big or too small
                 errors(2)
             elif tablecells[int(indexes)][cat] == 0: #cant select a tile that was already selected
                 errors(2)
+            
             else:
                 match indexes:
-                    case 1|2|3|4|5:
-                        question(cat, indexes-1)
-                        pBuzz = buzzer()
+                    case 1|2|3|4|5: 
+                        selected = question(cat, indexes-1) # Displays question, stores question text in a variable
+                        tiles[int(indexes)][cat] = 0 #Sets the tile on the board to 0 (which will clear it on refresh)
+                        pBuzz = buzzer() # The Buzzer!!!!!
                         
-                        if indexes == 1:
-                            col1[cat] = 0
-                        elif indexes == 2:
-                            col2[cat] = 0
-                        elif indexes == 3:
-                            col3[cat] = 0
-                        elif indexes == 4:
-                            col4[cat] = 0
-                        else:
-                            col5[cat] = 0
-                        
+                        while pBuzz != 3: # while pBuzz is 0, 1 or 2
+                            response = input("%s, your answer? " % players[pBuzz]) # prompts player for an answer
+                            responseCheck = False
+                            for i in range(len(answers[selected])): # checks all possible answers (list stored in a dict key named after the corresponding question)
+                                if answers[selected][i] == response:
+                                    responseCheck = True
+                                    break
+                                else:
+                                    pass
+                            
+                            if not responseCheck: # if the answer is wrong
+                                print("Regrettably, no.") # i heard alex trebek say this once and i thought it would be appropriate
+                                scores[pBuzz] = scores[pBuzz] - int(rnddiv*indexes) # subtracts score from player
+                                pBuzz = buzzer() # here we go again!
+                            
+                            else: # if the answer is correct
+                                print("That's correct!")
+                                break # break out of the while loop (no need for it for the question anymore)
+                                
                         if pBuzz == 3:
                             print("[### No answer! Moving on... ###]")
                         else: # if pBuzz is anything other than 0, 1, 2 or 3 we have bigger problems
                             scores[pBuzz] = scores[pBuzz] + int(rnddiv*indexes)#adds value of the tile to player's score
                             pBoard = pBuzz
-                        (tablecells, cellsum) = graphics(col1, col2, col3, col4, col5) #refreshes graphics to reflect the updated board
+                        (tablecells, cellsum) = graphics(tiles) #refreshes graphics to reflect the updated board
                     
                     case other: #you can't wager other values than the ones that are on the board
                         errors(2)
@@ -173,18 +188,17 @@ def buzzer(): # This function handles the "buzzer" for each answer.
             break
     return pBuzz
 
-def graphics(col1, col2, col3, col4, col5): #This function handles drawing the game's graphics.
+def graphics(tiles): #This function handles drawing the game's graphics.
    
     #These variables take pre-existing lists and arrange them into something usable
-    tablecells = [categories, col1, col2, col3, col4, col5 ]
     scorecells = [players, scores]
-    cellsum = np.sum(col1)+np.sum(col2)+np.sum(col3)+np.sum(col4)+np.sum(col5)
+    cellsum = np.sum(tiles[1]) + np.sum(tiles[2]) + np.sum(tiles[3]) + np.sum(tiles[4]) + np.sum(tiles[5])
     
     #base figure everything goes onto this thing
     plt.figure(figsize=(12,1),dpi=100,facecolor="#0567D2")
     
     #Draws the grid with all the answer values and everything
-    grid = plt.table(tablecells,loc='top',cellLoc='center',cellColours=gridcolours)
+    grid = plt.table(tiles,loc='top',cellLoc='center',cellColours=gridcolours)
     grid.scale(2,8)
     grid.auto_set_font_size(False)
     for i in range(0,6): #I have to individually set every single cell's text's formatting. fun
@@ -219,22 +233,24 @@ def graphics(col1, col2, col3, col4, col5): #This function handles drawing the g
     plt.gca().get_yaxis().set_visible(False)
     plt.box(on=None)
     plt.show()
-    return (tablecells, cellsum)
+    return(tiles, cellsum)
 
 def question(cat, indexes): # This function displays questions (technically answers but whatever)
-    text = np.array(questions[int(cat)][int(indexes)])
+    selected = questions[int(cat)][int(indexes)] # gets specified question
+    text = np.array(selected) # puts the text into an array to be put into a matplotlib table
     text.resize(1,1)
     plt.figure(figsize=(6,7),dpi=100,facecolor="#0567D2")
     displayQ = plt.table(text, loc='center', cellLoc='center', cellColours=np.full((1,1), "#010D8C"))
     displayQ.scale(2,26)
     displayQ.auto_set_font_size(False)
-    cellText = displayQ[(0,0)].get_text()
+    cellText = displayQ[(0,0)].get_text() #at least we only have to do this once instead of multiple times
     cellText.set_color("white")
     cellText.set_fontproperties(catSet)
     plt.gca().get_xaxis().set_visible(False)
     plt.gca().get_yaxis().set_visible(False)
     plt.box(on=None)
     plt.show()
+    return selected
 
 def errors(q): # This functions handles invalid inputs and what to return
     if q == 1:
